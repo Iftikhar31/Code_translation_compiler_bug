f503d473945c527f653362a9156fea28fbfe843a
Iftikhar31 <ikala005@ucr.edu>
2025-03-12 04:55:21 -0700

    Delete C++_Bugs/Bug1/cp_gimplify_vector.cpp

--- C++_Bugs/Bug1/cp_gimplify_vector.cpp
+++
@@ -1,6 +0,0 @@
-//The following program generates an ICE on trunk, but compiles on 14.2:
-#include <vector>
-int main() {
-   for (auto i : (true ? std::vector<int>{1, 2} : std::vector<int>{2, 1})) {
-   }
-}
