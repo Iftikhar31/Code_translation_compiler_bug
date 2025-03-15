59bed211e538129ad5aa440edce2d5809d536264
Aaron Tam <overdiisc@gmail.com>
2025-03-14 19:48:20 -0700

    Delete C#_Bugs directory

--- C#_Bugs/Bug3/indexer.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.CompilerServices;
-
-partial struct S1
-{
-    public partial int this[int x] {get=>x;}
-
-    [IndexerName("MyName")]
-    public partial int this[int x] {get;}
-}
